f01c37687b34a559d588a6f00e8c2887b01202a3
Joshtri <stuffofyos1516@gmail.com>
2024-11-27 11:16:52 +0800

    updating page, and item table

---
+++ app/items/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ItemTable from '@/components/items/item-table'
+
+export default function ItemPage() {
+  return (
+    <div>
+        <h2>Data Items</h2>
+        <ItemTable/>
+    </div>
+  )
+}
--- components/items/item-table.tsx
+++ components/items/item-table.tsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import { EditButton, DeleteButton } from "@/components/buttons";
 
-function itemTable() {
+function ItemTable() {
   return (
-    <div>item-table</div>
+    <table className="w-full text-sm text-left text-gray-500">
+      <thead className="text-sm text-gray-700 uppercase bg-gray-50">
+        <tr>
+          <th className="py-3 px-6">#</th>
+          <th className="py-3 px-6">Name</th>
+          <th className="py-3 px-6">Phone Number</th>
+          <th className="py-3 px-6">Created At</th>
+          <th className="py-3 px-6 text-center">Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        
+          <tr className="bg-white border-b">
+            <td className="py-3 px-6"></td>
+            <td className="py-3 px-6"></td>
+            <td className="py-3 px-6"></td>
+            <td className="py-3 px-6">
+             
+            </td>
+            <td className="flex justify-center gap-1 py-3">
+              {/* <EditButton id={contact.id} />
+              <DeleteButton id={contact.id} /> */}
+            </td>
+          </tr>
+      
+      </tbody>
+    </table>
   )
 }
 
-export default itemTable
+export default ItemTable
